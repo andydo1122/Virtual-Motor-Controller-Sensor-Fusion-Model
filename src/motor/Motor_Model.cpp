6f7dcadc0ecedee9125445ca7e151ce0c60199a7
doand <doand@XMC.com>
2026-02-27 19:54:17 -0600

    Worked out the physics of our simple Motor Model. Note: There is no torque load and no inductance calculated into this

--- src/motor/Motor_Model.cpp
+++ src/motor/Motor_Model.cpp
@@ -4,6 +4,7 @@
 //////////////////////////////////////////////
 
 #include "Motor_Model.hpp"
+#include "Motor_Params.hpp"
 namespace Motor
 {
 Motor_Model::Motor_Model() :
@@ -22,7 +23,26 @@ void Motor_Model::apply_voltage(double voltage)
 void Motor_Model::update(double dt)
 {
 
-    double accel     =  0.0; // TODO: implement physics
+    // Torque Physics //
+    // K_e(w)
+    double back_emf = MOTOR_KE * angular_velocity;
+
+    // i
+    double current = (voltage_input - back_emf) / MOTOR_RESISTANCE;
+
+    // t
+    double torque = MOTOR_KT * current;
+
+    // t_load
+    double load_torque = 0.0; // TODO, expand function later on.
+
+    // Equation of derivative of angular velocity (w) with respect to time (t) [dw/dt]
+    // Mechanical
+    // dw / dt
+    double accel = (torque - MOTOR_FRICTION * angular_velocity - load_torque)/MOTOR_INERTIA; 
+    // Torque Physics //
+    
+    // Update
     angular_velocity += accel * dt;
     angular_position += angular_velocity * dt;
 }
